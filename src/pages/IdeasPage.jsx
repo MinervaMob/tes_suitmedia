44422bc4d396ce6c2ac3f1546c5a98dc926b5796
Ilham Pangestu <140824958+MinervaMob@users.noreply.github.com>
2025-07-12 21:53:20 +0700

    update API

--- src/pages/IdeasPage.jsx
+++ src/pages/IdeasPage.jsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 import PostCard from '../components/PostCard';
 import Banner from '../components/Banner';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = 'https://suitmedia-backend.suitdev.com';
+
 
 export default function IdeasPage() {
   const [posts, setPosts] = useState([]);
