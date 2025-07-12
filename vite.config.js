e4f13d6530d82b1af3f4ba3c7230c3f9f0be051f
Ilham Pangestu <140824958+MinervaMob@users.noreply.github.com>
2025-07-12 22:09:44 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -8,18 +8,4 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://suitmedia-backend.suitdev.com',
-  //       changeOrigin: true,
-  //       rewrite: path => path.replace(/^\/api/, '/api'),
-  //     },
-  //     '/media': {
-  //       target: 'https://assets.suitdev.com',
-  //       changeOrigin: true,
-  //       rewrite: path => path.replace(/^\/media/, ''),
-  //     },
-  //   },
-  // },
 })
